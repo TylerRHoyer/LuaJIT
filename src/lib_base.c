f04e6a883ea9bfe8fb387e76b24a84cc02fa5327
Tyler <tyler@debian>
2019-04-24 01:32:02 -0600

    Removed functions

--- src/lib_base.c
+++ src/lib_base.c
@@ -371,17 +371,6 @@ static int load_aux(lua_State *L, int status, int envarg)
   }
 }
 
-LJLIB_CF(loadfile)
-{
-  GCstr *fname = lj_lib_optstr(L, 1);
-  GCstr *mode = lj_lib_optstr(L, 2);
-  int status;
-  lua_settop(L, 3);  /* Ensure env arg exists. */
-  status = luaL_loadfilex(L, fname ? strdata(fname) : NULL,
-			  mode ? strdata(mode) : NULL);
-  return load_aux(L, status, 3);
-}
-
 static const char *reader_func(lua_State *L, void *ud, size_t *size)
 {
   UNUSED(ud);
@@ -401,66 +390,8 @@ static const char *reader_func(lua_State *L, void *ud, size_t *size)
   }
 }
 
-LJLIB_CF(load)
-{
-  GCstr *name = lj_lib_optstr(L, 2);
-  GCstr *mode = lj_lib_optstr(L, 3);
-  int status;
-  if (L->base < L->top && (tvisstr(L->base) || tvisnumber(L->base))) {
-    GCstr *s = lj_lib_checkstr(L, 1);
-    lua_settop(L, 4);  /* Ensure env arg exists. */
-    status = luaL_loadbufferx(L, strdata(s), s->len, strdata(name ? name : s),
-			      mode ? strdata(mode) : NULL);
-  } else {
-    lj_lib_checkfunc(L, 1);
-    lua_settop(L, 5);  /* Reserve a slot for the string from the reader. */
-    status = lua_loadx(L, reader_func, NULL, name ? strdata(name) : "=(load)",
-		       mode ? strdata(mode) : NULL);
-  }
-  return load_aux(L, status, 4);
-}
-
-LJLIB_CF(loadstring)
-{
-  return lj_cf_load(L);
-}
-
-LJLIB_CF(dofile)
-{
-  GCstr *fname = lj_lib_optstr(L, 1);
-  setnilV(L->top);
-  L->top = L->base+1;
-  if (luaL_loadfile(L, fname ? strdata(fname) : NULL) != 0)
-    lua_error(L);
-  lua_call(L, 0, LUA_MULTRET);
-  return (int)(L->top - L->base) - 1;
-}
-
 /* -- Base library: GC control -------------------------------------------- */
 
-LJLIB_CF(gcinfo)
-{
-  setintV(L->top++, (G(L)->gc.total >> 10));
-  return 1;
-}
-
-LJLIB_CF(collectgarbage)
-{
-  int opt = lj_lib_checkopt(L, 1, LUA_GCCOLLECT,  /* ORDER LUA_GC* */
-    "\4stop\7restart\7collect\5count\1\377\4step\10setpause\12setstepmul");
-  int32_t data = lj_lib_optint(L, 2, 0);
-  if (opt == LUA_GCCOUNT) {
-    setnumV(L->top, (lua_Number)G(L)->gc.total/1024.0);
-  } else {
-    int res = lua_gc(L, opt, data);
-    if (opt == LUA_GCSTEP)
-      setboolV(L->top, res);
-    else
-      setintV(L->top, res);
-  }
-  L->top++;
-  return 1;
-}
 
 /* -- Base library: miscellaneous functions ------------------------------- */
 
